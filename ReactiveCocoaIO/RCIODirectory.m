e1e2bff49995054b2062f33e194e1eca3f09d725
Uri Baghin <skerfat@gmail.com>
2013-01-17 01:21:18 +0100

    Changed how children updates are propagated.

--- ReactiveCocoaIO/RCIODirectory.m
+++ ReactiveCocoaIO/RCIODirectory.m
@@ -9,15 +9,18 @@
 #import "RCIODirectory+Private.h"
 #import "RCIOItem+Private.h"
 
+static NSString * const RCIODirectoryChangeTypeAdd = @"RCIODirectoryChangeTypeAdd";
+static NSString * const RCIODirectoryChangeTypeRemove = @"RCIODirectoryChangeTypeRemove";
+
 @interface RCIODirectory ()
 
-@property (nonatomic, strong, readonly) NSArray *childrenBacking;
+@property (nonatomic, weak) RACSubject *childrenChannel;
+
+- (NSMutableArray *)loadChildren;
 
 @end
 
-@implementation RCIODirectory {
-	NSMutableArray *_childrenBacking;
-}
+@implementation RCIODirectory
 
 #pragma mark RCIOItem
 
@@ -43,7 +46,7 @@ static void processContent(NSArray *input, NSMutableArray *output, NSDirectoryEn
 			[output addObject:item];
 			
 			// Merge in descendants
-			if (!(options & NSDirectoryEnumerationSkipsSubdirectoryDescendants) && [item isKindOfClass:RCIODirectory.class]) processContent(((RCIODirectory *)item).childrenBacking, output, options, cancel);
+			if (!(options & NSDirectoryEnumerationSkipsSubdirectoryDescendants) && [item isKindOfClass:RCIODirectory.class]) processContent([(RCIODirectory *)item loadChildren], output, options, cancel);
 		}
 	}
 }
@@ -58,10 +61,26 @@ static void processContent(NSArray *input, NSMutableArray *output, NSDirectoryEn
 		[disposable addDisposable:[fileSystemScheduler() schedule:^{
 			@strongify(self);
 			
-			[disposable addDisposable:[[RACBind(self.childrenBacking) map:^ NSArray * (NSArray *content) {
-				IF_CANCELLED_RETURN(@[]);
-				NSMutableArray *processedContent = [NSMutableArray arrayWithCapacity:content.count];
+			RACSubject *childrenChannel = self.childrenChannel;
+			if (childrenChannel == nil) {
+				childrenChannel = [RACSubject subject];
+				self.childrenChannel = childrenChannel;
+			}
+			
+			[disposable addDisposable:[[[childrenChannel scanWithStart:[self loadChildren] combine:^id(NSMutableArray *children, RACTuple *change) {
+				RACTupleUnpack(NSString *type, RCIOItem *item) = change;
 				
+				if (type == RCIODirectoryChangeTypeAdd) {
+					[children addObject:item];
+				} else {
+					[children removeObject:item];
+				}
+				
+				return children;
+			}] map:^ NSArray * (NSArray *content) {
+				IF_CANCELLED_RETURN(@[]);
+				
+				NSMutableArray *processedContent = [NSMutableArray arrayWithCapacity:content.count];
 				processContent(content, processedContent, options, CANCELLATION_FLAG);
 				
 				return processedContent;
@@ -77,77 +96,28 @@ static void processContent(NSArray *input, NSMutableArray *output, NSDirectoryEn
 }
 
 - (void)didAddItem:(RCIOItem *)item {
-	NSParameterAssert(item != nil && ![self.childrenBacking containsObject:item]);
-	NSUInteger index = [self.childrenBacking indexOfObject:item inSortedRange:NSMakeRange(0, self.childrenBacking.count) options:NSBinarySearchingInsertionIndex usingComparator:^NSComparisonResult(RCIOItem *item1, RCIOItem *item2) {
-		return [item1.urlBacking.lastPathComponent compare:item2.urlBacking.lastPathComponent options:NSCaseInsensitiveSearch | NSForcedOrderingSearch];
-	}];
-	[[self mutableArrayValueForKey:@keypath(self.childrenBacking)] insertObject:item atIndex:index];
+	ASSERT_FILE_SYSTEM_SCHEDULER();
+	NSParameterAssert(item != nil);
+	[self.childrenChannel sendNext:[RACTuple tupleWithObjects:RCIODirectoryChangeTypeAdd, item, nil]];
 }
 
 - (void)didRemoveItem:(RCIOItem *)item {
-	NSParameterAssert(item != nil && [self.childrenBacking containsObject:item]);
-	[[self mutableArrayValueForKey:@keypath(self.childrenBacking)] removeObject:item];
-}
-
-#pragma mark Private Methods
-
-- (NSArray *)childrenBacking {
 	ASSERT_FILE_SYSTEM_SCHEDULER();
-	
-	if (_childrenBacking == nil) {
-		NSURL *url = self.urlBacking;
-		if (url == nil) {
-			return nil;
-		}
-		
-		_childrenBacking = [NSMutableArray array];
-		for (NSURL *childURL in [NSFileManager.defaultManager enumeratorAtURL:url includingPropertiesForKeys:@[NSURLFileResourceTypeKey] options:NSDirectoryEnumerationSkipsSubdirectoryDescendants errorHandler:nil]) {
-			RCIOItem *child = [self.class loadItemFromURL:childURL];
-			if (child != nil) [_childrenBacking addObject:child];
-		}
+	NSParameterAssert(item != nil);
+	[self.childrenChannel sendNext:[RACTuple tupleWithObjects:RCIODirectoryChangeTypeRemove, item, nil]];
+}
+
+#pragma mark - Private Methods
+
+- (NSMutableArray *)loadChildren {
+	ASSERT_FILE_SYSTEM_SCHEDULER();
+	if (self.urlBacking == nil) return nil;
+	NSMutableArray *children = [NSMutableArray array];
+	for (NSURL *url in [NSFileManager.defaultManager enumeratorAtURL:self.urlBacking includingPropertiesForKeys:nil options:NSDirectoryEnumerationSkipsSubdirectoryDescendants errorHandler:nil]) {
+		RCIOItem *child = [RCIOItem loadItemFromURL:url];
+		if (child != nil) [children addObject:child];
 	}
-	
-	return _childrenBacking;
-}
-
-- (NSUInteger)countOfChildrenBacking {
-	return _childrenBacking.count;
-}
-
-- (RCIOItem *)objectInChildrenBackingAtIndex:(NSUInteger)index {
-	return [_childrenBacking objectAtIndex:index];
-}
-
-- (NSArray *)childrenBackingAtIndexes:(NSIndexSet *)indexes {
-	return [_childrenBacking objectsAtIndexes:indexes];
-}
-
-- (void)getChildrenBacking:(RCIOItem * __unsafe_unretained *)buffer range:(NSRange)inRange {
-	[_childrenBacking getObjects:buffer range:inRange];
-}
-
-- (void)insertObject:(RCIOItem *)object inChildrenBackingAtIndex:(NSUInteger)index {
-	[_childrenBacking insertObject:object atIndex:index];
-}
-
-- (void)insertChildrenBacking:(NSArray *)array atIndexes:(NSIndexSet *)indexes {
-	[_childrenBacking insertObjects:array atIndexes:indexes];
-}
-
-- (void)removeObjectFromChildrenBackingAtIndex:(NSUInteger)index {
-	[_childrenBacking removeObjectAtIndex:index];
-}
-
-- (void)removeChildrenBackingAtIndexes:(NSIndexSet *)indexes {
-	[_childrenBacking removeObjectsAtIndexes:indexes];
-}
-
-- (void)replaceObjectInChildrenBackingAtIndex:(NSUInteger)index withObject:(RCIOItem *)object {
-	[_childrenBacking replaceObjectAtIndex:index withObject:object];
-}
-
-- (void)replaceChildrenBackingAtIndexes:(NSIndexSet *)indexes withChildrenBacking:(NSArray *)array {
-	[_childrenBacking replaceObjectsAtIndexes:indexes withObjects:array];
+	return children;
 }
 
 @end
