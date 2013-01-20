7ec873e2269fb5400e4d3a8fad03082b5960027b
Uri Baghin <skerfat@gmail.com>
2013-01-20 17:24:05 +0100

    Added tests for NSURL+TrailingSlash to account for NSURL magic.

--- ReactiveCocoaIOTests/NSURLTrailingSlashSpec.m
+++ ReactiveCocoaIOTests/NSURLTrailingSlashSpec.m
@@ -13,16 +13,32 @@ SpecBegin(NSURLTrailingSlash)
 describe(@"NSURL+TrailingSlash", ^{
 	__block NSURL *urlWithoutTrailingSlash;
 	__block NSURL *urlWithTrailingSlash;
+	__block NSURL *fileURLWithoutTrailingSlash;
+	__block NSURL *fileURLWithTrailingSlash;
+	__block NSURL *directoryURLWithoutTrailingSlash;
+	__block NSURL *directoryURLWithTrailingSlash;
 	
 	beforeAll(^{
-		// Create URLs for files that don't exist on the file system to avoid NSURL magic
+		// URLs for files that don't exist on the file system to avoid NSURL magic
 		urlWithoutTrailingSlash = [NSURL URLWithString:@"file://localhost/directorythatdoesntexist/test"];
 		urlWithTrailingSlash = [NSURL URLWithString:@"file://localhost/directorythatdoesntexist/test/"];
+		// URLs for files that exist on the file system to trigger NSURL magic
+		fileURLWithoutTrailingSlash = [NSURL URLWithString:@"file://localhost/etc/hosts"];
+		fileURLWithTrailingSlash = [NSURL URLWithString:@"file://localhost/etc/hosts/"];
+		directoryURLWithoutTrailingSlash = [NSURL URLWithString:@"file://localhost/etc"];
+		directoryURLWithTrailingSlash = [NSURL URLWithString:@"file://localhost/etc/"];
 	});
 	
 	it(@"should detect a trailing slash", ^{
 		expect(urlWithoutTrailingSlash.hasTrailingSlash).to.beFalsy();
 		expect(urlWithTrailingSlash.hasTrailingSlash).to.beTruthy();
+	});
+	
+	it(@"should detect a trailing slash in file urls", ^{
+		expect(fileURLWithoutTrailingSlash.hasTrailingSlash).to.beFalsy();
+		expect(fileURLWithTrailingSlash.hasTrailingSlash).to.beTruthy();
+		expect(directoryURLWithoutTrailingSlash.hasTrailingSlash).to.beFalsy();
+		expect(directoryURLWithTrailingSlash.hasTrailingSlash).to.beTruthy();
 	});
 	
 	it(@"should remove trailing slashes from urls with trailing slashes", ^{
@@ -39,6 +55,38 @@ describe(@"NSURL+TrailingSlash", ^{
 	
 	it(@"should not add trailing slashes to urls with trailing slashes", ^{
 		expect(urlWithTrailingSlash.URLByAppendingTrailingSlash).to.equal(urlWithTrailingSlash);
+	});
+	
+	it(@"should remove trailing slashes from file urls with trailing slashes", ^{
+		expect(fileURLWithTrailingSlash.URLByDeletingTrailingSlash).to.equal(fileURLWithoutTrailingSlash);
+	});
+	
+	it(@"should not remove trailing slashes from file urls without trailing slashes", ^{
+		expect(fileURLWithoutTrailingSlash.URLByDeletingTrailingSlash).to.equal(fileURLWithoutTrailingSlash);
+	});
+	
+	it(@"should add trailing slashes to file urls without trailing slashes", ^{
+		expect(fileURLWithoutTrailingSlash.URLByAppendingTrailingSlash).to.equal(fileURLWithTrailingSlash);
+	});
+	
+	it(@"should not add trailing slashes to file urls with trailing slashes", ^{
+		expect(fileURLWithTrailingSlash.URLByAppendingTrailingSlash).to.equal(fileURLWithTrailingSlash);
+	});
+
+	it(@"should remove trailing slashes from directory urls with trailing slashes", ^{
+		expect(directoryURLWithTrailingSlash.URLByDeletingTrailingSlash).to.equal(directoryURLWithoutTrailingSlash);
+	});
+	
+	it(@"should not remove trailing slashes from directory urls without trailing slashes", ^{
+		expect(directoryURLWithoutTrailingSlash.URLByDeletingTrailingSlash).to.equal(directoryURLWithoutTrailingSlash);
+	});
+	
+	it(@"should add trailing slashes to directory urls without trailing slashes", ^{
+		expect(directoryURLWithoutTrailingSlash.URLByAppendingTrailingSlash).to.equal(directoryURLWithTrailingSlash);
+	});
+	
+	it(@"should not add trailing slashes to directory urls with trailing slashes", ^{
+		expect(directoryURLWithTrailingSlash.URLByAppendingTrailingSlash).to.equal(directoryURLWithTrailingSlash);
 	});
 });
 
