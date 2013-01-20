fd4eda40589e33b063429e8da798c73f79ec2a85
Uri Baghin <skerfat@gmail.com>
2013-01-20 17:31:24 +0100

    Reimplemented NSURL+TrailingSlash to account for NSURL magic.

--- ReactiveCocoaIO/NSURL+TrailingSlash.m
+++ ReactiveCocoaIO/NSURL+TrailingSlash.m
@@ -8,6 +8,9 @@
 
 #import "NSURL+TrailingSlash.h"
 
+// Can't use +[NSURL fileURLWithPath:] or -[NSURL path] for these because they
+// have a lot of magic in them to add / remove trailing slashes depending on
+// whether they're pointing to a file or directory.
 @implementation NSURL (TrailingSlash)
 
 - (BOOL)hasTrailingSlash {
@@ -18,14 +21,14 @@
 - (NSURL *)URLByAppendingTrailingSlash {
 	NSParameterAssert(self.isFileURL);
 	NSURL *url = self;
-	if (!self.hasTrailingSlash) url = [NSURL fileURLWithPath:[url.path stringByAppendingString:@"/"]];
+	if (!self.hasTrailingSlash) url = [NSURL URLWithString:[NSString stringWithFormat:@"file://localhost%@/", self.path]];
 	return url;
 }
 
 - (NSURL *)URLByDeletingTrailingSlash {
 	NSParameterAssert(self.isFileURL);
 	NSURL *url = self;
-	if (self.hasTrailingSlash) url = [NSURL fileURLWithPath:[url.path substringToIndex:url.path.length]];
+	if (self.hasTrailingSlash) url = [NSURL URLWithString:[NSString stringWithFormat:@"file://localhost%@", [url.path substringToIndex:url.path.length]]];
 	return url;
 }
 
