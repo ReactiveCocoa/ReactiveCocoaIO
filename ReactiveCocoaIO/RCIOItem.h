79a9a4fab244841356df770d6a609291261c137e
Uri Baghin <skerfat@gmail.com>
2013-11-10 15:27:19 +0100

    Removed file management methods from RCIOItem.

--- ReactiveCocoaIO/RCIOItem.h
+++ ReactiveCocoaIO/RCIOItem.h
@@ -50,49 +50,6 @@ typedef enum : NSUInteger {
 
 @end
 
-@interface RCIOItem (FileManagement)
-
-// Moves the receiver to the given directory.
-//
-// destination   - An optional RCIODirectory to which the receiver is
-//                 moved.
-// newName       - An optional name to rename the file to.
-// shouldReplace - Indicates if the operation should replace any existing file.
-//
-// Returns a signal that sends the moved RCIOItem and completes.
-- (RACSignal *)moveTo:(RCIODirectory *)destination withName:(NSString *)newName replaceExisting:(BOOL)shouldReplace;
-
-// Copy the receiver to the given directory.
-//
-// destination   - An optional RCIODirectory to which the receiver is
-//                 copied.
-// newName       - An optional name to rename the file to.
-// shouldReplace - Indicates if the operation should replace any existing file.
-//
-// Returns a signal that sends the newly copied RCIOItem and completes.
-- (RACSignal *)copyTo:(RCIODirectory *)destination withName:(NSString *)newName replaceExisting:(BOOL)shouldReplace;
-
-// Equivalent to -moveTo:destination withName:nil replaceExisting:YES.
-- (RACSignal *)moveTo:(RCIODirectory *)destination;
-
-// Equivalent to -copyTo:destination withName:nil replaceExisting:YES.
-- (RACSignal *)copyTo:(RCIODirectory *)destination;
-
-// Equivalent to -moveTo:nil withName:newName replaceExisting:YES.
-- (RACSignal *)renameTo:(NSString *)newName;
-
-// Duplicates the receiver.
-//
-// Returns a signal that sends the duplicate and completes.
-- (RACSignal *)duplicate;
-
-// Deletes the receiver.
-//
-// Returns a signal that sends the deleted item and completed.
-- (RACSignal *)delete;
-
-@end
-
 @interface RCIOItem (ExtendedAttributes)
 
 // Returns a channel terminal for the receiver's extended attribute identified
