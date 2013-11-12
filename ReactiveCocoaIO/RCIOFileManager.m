d65b2f241ab747be6ceb0e11b0d283b486ec9c09
Uri Baghin <skerfat@gmail.com>
2013-11-13 00:43:54 +0100

    Added naive implementation of RCIOFileManager.

--- ReactiveCocoaIO/RCIOFileManager.m
+++ ReactiveCocoaIO/RCIOFileManager.m
@@ -13,19 +13,52 @@
 @implementation RCIOFileManager
 
 + (RACSignal *)contentsOfDirectoryAtURL:(NSURL *)url options:(NSDirectoryEnumerationOptions)options {
-	return RACSignal.empty;
+	return [RACSignal createSignal:^ RACDisposable * (id<RACSubscriber> subscriber) {
+		NSDirectoryEnumerator *enumerator = [[[NSFileManager alloc] init] enumeratorAtURL:url includingPropertiesForKeys:nil options:options errorHandler:nil];
+
+		[subscriber sendNext:enumerator.rac_promise.deferred];
+
+		return nil;
+	}];
 }
 
 + (RACSignal *)moveItemAtURL:(NSURL *)sourceURL toURL:(NSURL *)destinationURL {
-	return RACSignal.empty;
+	return [RACSignal createSignal:^ RACDisposable * (id<RACSubscriber> subscriber) {
+		NSError *error;
+		BOOL success = [[[NSFileManager alloc] init] moveItemAtURL:sourceURL toURL:destinationURL error:&error];
+		if (success) {
+			[subscriber sendCompleted];
+		} else {
+			[subscriber sendError:error];
+		}
+		return nil;
+	}];
 }
 
 + (RACSignal *)copyItemAtURL:(NSURL *)sourceURL toURL:(NSURL *)destinationURL {
-	return RACSignal.empty;
+	return [RACSignal createSignal:^ RACDisposable * (id<RACSubscriber> subscriber) {
+		NSError *error;
+		BOOL success = [[[NSFileManager alloc] init] copyItemAtURL:sourceURL toURL:destinationURL error:&error];
+		if (success) {
+			[subscriber sendCompleted];
+		} else {
+			[subscriber sendError:error];
+		}
+		return nil;
+	}];
 }
 
 + (RACSignal *)removeItemAtURL:(NSURL *)url {
-	return RACSignal.empty;
+	return [RACSignal createSignal:^ RACDisposable * (id<RACSubscriber> subscriber) {
+		NSError *error;
+		BOOL success = [[[NSFileManager alloc] init] removeItemAtURL:url error:&error];
+		if (success) {
+			[subscriber sendCompleted];
+		} else {
+			[subscriber sendError:error];
+		}
+		return nil;
+	}];
 }
 
 @end
